63bceee952fa51f4fcfba52ad0dc57ca262afdda
Milen Chavdarov <mchavdarov@milens-macbook-air.local>
2016-03-13 03:37:37 +0200

    Part-5 Modified

--- MusicVideos/MusicVideos.swift
+++ MusicVideos/MusicVideos.swift
@@ -10,14 +10,30 @@ import Foundation
 
 class Videos {
 
-    private var _vName: String
-    private var _vImageUrl: String
-    private var _vVideoUrl: String
+    private var _vName:         String
+    private var _vRights:       String
+    private var _vPrice:        String
+    private var _vCurrency:     String
+    private var _vImageUrl:     String
+    private var _vArtist:       String
+    private var _vVideoUrl:     String
+    private var _vImid:         String
+    private var _vGenre:        String
+    private var _vLinkToiTunes: String
+    private var _vReleaseDte:   String
     
     //Make geters
-    var vName: String{ return _vName }
-    var vImageUrl: String { return _vImageUrl }
-    var vVideoUrl: String { return _vVideoUrl }
+    var vName:          String{ return _vName }
+    var vRights:        String{ return _vRights }
+    var vPrice:         String{ return _vPrice }
+    var vCurency:       String{ return _vCurrency }
+    var vImageUrl:      String{ return _vImageUrl }
+    var vArtist:        String{ return _vArtist }
+    var vVideoUrl:      String{ return _vVideoUrl }
+    var vImId:          String{ return _vImid }
+    var vGenre:         String{ return _vGenre }
+    var vLinkToItunes:  String{ return _vLinkToiTunes }
+    var vReleaseDate:   String{ return _vReleaseDte }
     
     init ( data: JSONDictionary ) {
     
@@ -26,26 +42,80 @@ class Videos {
                 vName = name["label"] as? String{
                     self._vName = vName
         } else{
-            _vName = ""
+            self._vName = ""
+        }
+        
+        if let rights = data["rights"] as? JSONDictionary,
+                vRights = rights["label"] as? String {
+                    self._vRights = vRights
+        } else {
+            self._vRights = ""
+        }
+        
+        if let price = data["im:price"] as? JSONDictionary,
+                vPrice = price["attributes"] as? JSONDictionary,
+                prc = vPrice["amount"] as? String,
+                curr = vPrice["currency"] as? String {
+                    self._vPrice = prc
+                    self._vCurrency = curr
+        } else {
+            self._vPrice = ""
+            self._vCurrency = ""
         }
         
         //Video Image
         if let img = data["im:image"] as? JSONArray,
                 image = img[2] as? JSONDictionary,
                 immage = image["label"] as? String {
-                    _vImageUrl = immage.stringByReplacingOccurrencesOfString( "100x100", withString: "600x600" )
+                    self._vImageUrl = immage.stringByReplacingOccurrencesOfString( "100x100", withString: "600x600" )
         } else {
-            _vImageUrl = ""
+            self._vImageUrl = ""
         }
     
+        if let artist = data["im:artist"] as? JSONDictionary,
+                artLabel = artist["label"] as? String {
+                    self._vArtist = artLabel
+        } else {
+            self._vArtist = ""
+        }
+        
+        if let imid = data["id"] as? JSONDictionary,
+            idAttrib = imid["attributes"] as? JSONDictionary,
+            id = idAttrib["im:id"] as? String {
+                self._vImid = id
+        } else {
+            self._vImid = ""
+        }
+        
+        if let genr = data["category"] as? JSONDictionary,
+                genrAttrib = genr["atributes"] as? JSONDictionary,
+                genre = genrAttrib["term"] as? String {
+                    self._vGenre = genre
+        } else {
+                self._vGenre = ""
+        }
+        
+        if let relDte = data["im:releaseDate"] as? JSONDictionary,
+            dteAttrib = relDte["attributes"] as? JSONDictionary,
+            dte = dteAttrib["label"] as? String {
+                _vReleaseDte = dte
+        } else {
+            _vReleaseDte = ""
+        }
+        
         //Video URL
         if let video = data["link"] as? JSONArray,
                 vUrl = video[1] as? JSONDictionary,
                 vHref = vUrl["attributes"] as? JSONDictionary,
-                vVideoUrl = vHref["href"] as? String {
-                    self._vVideoUrl = vVideoUrl
+                vVideoUrl = vHref["href"] as? String,
+                viTunes = video[0] as? JSONDictionary,
+                viTunees = viTunes["attributes"] as? JSONDictionary,
+                viTuneees = viTunees["href"] as? String {
+                    _vVideoUrl = vVideoUrl
+                    _vLinkToiTunes = viTuneees
         } else {
             _vVideoUrl = ""
+            _vLinkToiTunes = ""
         }
     }
 }
